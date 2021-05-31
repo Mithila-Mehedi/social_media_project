2471c7112bed6f38a341dadbb6232874a46d0b03
Mithila <mithilamehedi60@gmail.com>
2021-05-31 19:59:43 +0600

    CRUD with express

---
+++ routes/users.js
@@ -0,0 +1,56 @@
+const router = require('express').Router()
+const User = require('../models/User')
+const bcrypt = require('bcrypt')
+
+// router.get('/', (req,res) => {
+//     res.send("hey its user route")
+// })
+
+
+//update user
+router.put('/:id', async (req,res) => {
+    if(req.body.userID === req.params.id || req.body.isAdmin){
+        if(req.body.password){
+            try{
+                const salt = await bcrypt.genSalt(10)
+                req.body.password = await bcrypt.hash(req.body.password,salt)
+            } catch(err){
+                res.json(err)
+            }
+        }
+    
+        try{
+            const user = await User.findByIdAndUpdate(req.params.id, {
+                $set: req.body,
+            })
+            res.status(200).json("Account has been updated")
+        } catch(err){
+            res.json(err)
+        }
+    }
+        else{
+            return res.status(403).json("you can update only your account")
+        }
+    
+})
+
+
+///delete user
+
+router.delete('/:id', async (req,res) => {
+    if(req.body.userID === req.params.id || req.body.isAdmin){
+         try{
+            const user = await User.findByIdAndDelete(req.params.id)
+            res.status(200).json("Account has been deleted")
+        } catch(err){
+            res.json(err)
+        }
+    }
+        else{
+            return res.status(403).json("you can update only your account")
+        }
+    
+})
+
+
+module.exports = router
